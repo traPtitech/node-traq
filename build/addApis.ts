036b6441e9a0a1fc22acc8a89d352ed0f15ca598
sapphi-red <green@sapphi.red>
2019-07-22 17:04:41 +0900

    Update: oneOfに対応

--- build/addApis.ts
+++ build/addApis.ts
@@ -1,19 +1,57 @@
-import { Project, MethodDeclarationStructure } from "ts-morph";
-import "array-flat-polyfill";
+import {
+  Project,
+  MethodDeclarationStructure,
+  SyntaxKind,
+  ts,
+  SourceFile
+} from "ts-morph"
+import "array-flat-polyfill"
 
-export default async function(generatedFolder: string) {
-  const project = new Project();
-  project.addExistingSourceFiles(`${generatedFolder}/**/*.ts`);
+const transformOneOf = async (sourceFile: SourceFile) => {
+  const interfaceProps = sourceFile
+    .getInterfaces()
+    .flatMap(i => i.getProperties())
 
-  const sourceFile = project.getSourceFileOrThrow("api.ts");
+  for (const prop of interfaceProps) {
+    const oneOfTypeNodes = prop
+      .getChildrenOfKind(SyntaxKind.TypeReference)
+      .concat(
+        prop
+          .getChildrenOfKind(SyntaxKind.JSDocComment)
+          .flatMap(comment =>
+            comment.getChildrenOfKind(SyntaxKind.JSDocTypeTag)
+          )
+          .flatMap(typeTag =>
+            typeTag.getChildrenOfKind(SyntaxKind.JSDocTypeExpression)
+          )
+          .flatMap(exp => exp.getChildrenOfKind(SyntaxKind.TypeReference))
+      )
+      .filter(node => node.getText().startsWith("OneOf"))
 
-  const classes = sourceFile.getClasses();
+    for (const node of oneOfTypeNodes) {
+      node.transform(traversal => {
+        const node = traversal.currentNode
+        return ts.createUnionTypeNode(
+          node
+            .getText()
+            .slice("OneOf".length)
+            .split("Event")
+            .map(typeName => ts.createTypeReferenceNode(`${typeName}Event`, undefined))
+        )
+      })
+    }
+  }
+  await sourceFile.save()
+}
+
+const generateBaseAPI = async (sourceFile: SourceFile) => {
+  const classes = sourceFile.getClasses()
   const apiClasses = classes.filter(c => {
-    const baseClass = c.getBaseClass();
-    if (!baseClass) return false;
-    return baseClass.getName() === "BaseAPI";
-  });
-  const apiMethods = apiClasses.map(c => c.getMethods()).flat();
+    const baseClass = c.getBaseClass()
+    if (!baseClass) return false
+    return baseClass.getName() === "BaseAPI"
+  })
+  const apiMethods = apiClasses.map(c => c.getMethods()).flat()
 
   sourceFile.addClass({
     name: "Apis",
@@ -22,7 +60,17 @@ export default async function(generatedFolder: string) {
       m => m.getStructure() as MethodDeclarationStructure
     ),
     isExported: true
-  });
+  })
 
-  await sourceFile.save();
+  await sourceFile.save()
+}
+
+export default async function(generatedFolder: string) {
+  const project = new Project()
+  project.addExistingSourceFiles(`${generatedFolder}/**/*.ts`)
+
+  const sourceFile = project.getSourceFileOrThrow("api.ts")
+
+  await transformOneOf(sourceFile)
+  await generateBaseAPI(sourceFile)
 }
