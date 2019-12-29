71ae68b43f116fdfc87c68ffd048b8152ecacd15
sapphi-red <green@sapphi.red>
2019-12-29 21:25:33 +0900

    Clean: remove transformOneOf

--- build/addApis.ts
+++ build/addApis.ts
@@ -1,48 +1,9 @@
 import {
   Project,
   MethodDeclarationStructure,
-  SyntaxKind,
-  ts,
   SourceFile
 } from "ts-morph"
 import "array-flat-polyfill"
-
-const transformOneOf = async (sourceFile: SourceFile) => {
-  const interfaceProps = sourceFile
-    .getInterfaces()
-    .flatMap(i => i.getProperties())
-
-  for (const prop of interfaceProps) {
-    const oneOfTypeNodes = prop
-      .getChildrenOfKind(SyntaxKind.TypeReference)
-      .concat(
-        prop
-          .getChildrenOfKind(SyntaxKind.JSDocComment)
-          .flatMap(comment =>
-            comment.getChildrenOfKind(SyntaxKind.JSDocTypeTag)
-          )
-          .flatMap(typeTag =>
-            typeTag.getChildrenOfKind(SyntaxKind.JSDocTypeExpression)
-          )
-          .flatMap(exp => exp.getChildrenOfKind(SyntaxKind.TypeReference))
-      )
-      .filter(node => node.getText().startsWith("OneOf"))
-
-    for (const node of oneOfTypeNodes) {
-      node.transform(traversal => {
-        const node = traversal.currentNode
-        return ts.createUnionTypeNode(
-          node
-            .getText()
-            .slice("OneOf".length)
-            .split("Event")
-            .map(typeName => ts.createTypeReferenceNode(`${typeName}Event`, undefined))
-        )
-      })
-    }
-  }
-  await sourceFile.save()
-}
 
 const generateBaseAPI = async (sourceFile: SourceFile) => {
   const classes = sourceFile.getClasses()
@@ -71,6 +32,5 @@ export default async function(generatedFolder: string) {
 
   const sourceFile = project.getSourceFileOrThrow("api.ts")
 
-  await transformOneOf(sourceFile)
   await generateBaseAPI(sourceFile)
 }
