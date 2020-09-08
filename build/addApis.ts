b8067820ccf99d5455ec94d99ecd2e159b4c1261
sapphi-red <green@sapphi.red>
2020-09-08 15:23:57 +0900

    Remove patch for typescript@4.0

--- build/addApis.ts
+++ build/addApis.ts
@@ -2,10 +2,6 @@ import {
   Project,
   MethodDeclarationStructure,
   SourceFile,
-  ts,
-  ObjectLiteralExpression,
-  PropertyAssignment,
-  ExpressionStatement
 } from "ts-morph"
 
 const generateBaseAPI = async (sourceFile: SourceFile) => {
@@ -37,43 +33,11 @@ const generateBaseAPI = async (sourceFile: SourceFile) => {
   await sourceFile.save()
 }
 
-const ignoreErrorWithDelete = async (sourceFile: SourceFile) => {
-  const creatorFuncs = sourceFile
-    .getVariableDeclarations()
-    .filter(d => d.getName().endsWith('Creator'))
-
-  const returns = creatorFuncs
-    .map(d => d.getInitializerIfKindOrThrow(ts.SyntaxKind.FunctionExpression))
-    .flatMap(f => f.getBody().getChildrenOfKind(ts.SyntaxKind.ReturnStatement))
-
-  const properties = returns
-    .map(r => r.getExpressionOrThrow())
-    .filter((e): e is ObjectLiteralExpression =>
-      e.getKind() === ts.SyntaxKind.ObjectLiteralExpression
-    )
-    .flatMap(o => o.getProperties())
-    .filter((p): p is PropertyAssignment => p.getKind() === ts.SyntaxKind.PropertyAssignment)
-
-  const deletes = properties
-    .map(p => p.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrowFunction))
-    .flatMap(i => i.getBody().getChildrenOfKind(ts.SyntaxKind.ExpressionStatement))
-    .flatMap(es => es.getChildrenOfKind(ts.SyntaxKind.DeleteExpression))
-
-  deletes.forEach(d => {
-    const i = d.getChildIndex()
-    const parent = d.getParentIfKindOrThrow(ts.SyntaxKind.ExpressionStatement)
-    parent.insertJsDoc(i, '@ts-ignore')
-  })
-
-  await sourceFile.save()
-}
-
 export default async function(generatedFolder: string) {
   const project = new Project()
   project.addSourceFilesAtPaths(`${generatedFolder}/**/*.ts`)
 
   const sourceFile = project.getSourceFileOrThrow("api.ts")
 
-  await ignoreErrorWithDelete(sourceFile)
   await generateBaseAPI(sourceFile)
 }
