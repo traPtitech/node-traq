2b2e00a3a279c008b2dec7b3823bd0a91cb93367
sapphi-red <green@sapphi.red>
2022-03-10 13:04:25 +0900

    Clean: use tsup & esno

--- build/index.ts
+++ build/index.ts
@@ -5,9 +5,6 @@ const SWAGGER_URL =
   "https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml";
 
 const GENERATED_FOLDER = "./bin/generated";
-const DIST_ES_FOLDER = "./dist/es";
-const DIST_COMMONJS_FOLDER = "./dist/commonjs";
-const DIST_TYPE_FOLDER = "./dist/types";
 
 const npx = process.platform === "win32" ? "npx.cmd" : "npx";
 
@@ -25,15 +22,6 @@ const generateCmd = [
   "--reserved-words-mappings public=public"
 ];
 
-const baseTscCmd = [
-  "tsc",
-  `${GENERATED_FOLDER}/index.ts`,
-  "-t",
-  "ES2019",
-  "--moduleResolution",
-  "node"
-];
-
 (async () => {
   try {
     await runCmdAsync(npx, generateCmd);
@@ -41,28 +29,6 @@ const baseTscCmd = [
     console.log("Start adding Apis class...");
     await addApis(GENERATED_FOLDER);
     console.log("Finished adding Apis class");
-
-    console.log("Start building ESModule version");
-    await runCmdAsync(
-      npx,
-      baseTscCmd.concat([
-        "-m",
-        "es2015",
-        "--outDir",
-        DIST_ES_FOLDER,
-        "-d",
-        "--declarationDir",
-        DIST_TYPE_FOLDER
-      ])
-    );
-    console.log("Finished building ESModule version");
-
-    console.log("Start building CommonJS version");
-    await runCmdAsync(
-      npx,
-      baseTscCmd.concat(["-m", "commonjs", "--outDir", DIST_COMMONJS_FOLDER])
-    );
-    console.log("Finished building CommonJS version");
   } catch (e) {
     console.error(e);
     process.exit(1);
---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['bin/generated/index.ts'],
+  format: ['cjs', 'esm'],
+  target: 'es2020',
+  dts: true,
+  sourcemap: true
+})
